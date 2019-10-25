7bd28c183edd4af685183ec42acbf274b6f9aab6
MyatTheingi-mhs <myattheingi.mhs@gmail.com>
2019-10-25 14:18:06 +0630

    change Text

---
+++ app/src/main/java/com/example/kotlintutorialtest/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.kotlintutorialtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        txt_Hello.text="welcome to Android Studio"
+
+    }
+}
